b7f4c9b74665fad8f886408d0d12891ca0a02e94
patidardashrath42 <patidardashrath42@gmail.com>
2024-06-06 14:32:01 +0530

    setup is running

--- Readme.md
+++ Readme.md
@@ -1,1 +1,8 @@
 backend with node js learning ...
+
+
+in package json file , whenver we are using type : module 
+then we will always using import or 
+type: commonjs in that case 
+we will use require in all cases.
+
---
+++ src/db/index.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async () => {
+    try{
+       const connectionConfiguration = await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
+       console.log('MoNGODB is connected !!!', connectionConfiguration.connection.host);
+    }catch(error){
+        console.log('error: ', error)
+        process.exit(1);
+    }
+}
+
+export default connectDB;
