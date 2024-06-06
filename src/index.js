5e9115defa0cfa60f04baa6d39cb9609b3cd0e8b
patidardashrath42 <patidardashrath42@gmail.com>
2024-06-06 23:12:05 +0530

    making some utills

--- src/index.js
+++ src/index.js
@@ -1,31 +1,18 @@
 import dotenv from "dotenv";
-import express from 'express';
-import cors from 'cors';
 import connectDB from './db/index.js';
 
 dotenv.config({
    path: './env'
 })
 
- const app = express();
- const port = process.env.port || 3000;   
- app.use(cors());
 
-connectDB();
-
- let user = [
-    { "name": "Arjun Tripathi", "email": "d1@gmail.com", "password": "123456", "gender": "m", "id": "1"},
-    { "name": "Rahul Durgapal", "email": "d2@gmail.com", "password": "12345", "gender": "m", "id": "2" },
-    { "name": "priti", "email": "d3@gmail.com", "password": "1234", "gender": "f", "id": "3"}
-];
-
-app.get('/', (req, res) => {
-    res.send('Hello');
- })
-app.get('/user', (req, res) => {
-   res.status(200).json(user);
+connectDB()
+.then(()=> {
+   app.listen(process.env.PORT || 3000, ()=> {
+      console.log(`server is running port : ${process.env.PORT}`);
+   })
 })
+.catch((err) => {
+   console.log('connection failed', err)
+});
 
-app.listen(port, () => {
-     console.log(`Example app listening at http://localhost:${port}`)
- })
